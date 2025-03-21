534360fced8c0048125bbe4446fc98e1a46aab2e
Anna Borisova <anna.boris_va@bk.ru>
2025-03-21 21:53:59 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -82,69 +82,69 @@ FROM tbl
 ORDER BY 4 DESC;
 
 --CPU, CLP, CPPU, ROI
-with ranked_clicks as (
-    select
+WITH ranked_clicks AS (
+    SELECT
         s.visitor_id,
         s.visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
         l.lead_id,
         l.amount,
         l.closing_reason,
         l.status_id,
-        ROW_NUMBER() over (
-            partition by s.visitor_id
-            order by s.visit_date desc
-        ) as rn
-    from sessions as s
-    left join
-        leads as l
-        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-    where s.medium <> 'organic'
+        ROW_NUMBER() OVER (
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM sessions AS s
+    LEFT JOIN
+        leads AS l
+        ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
+    WHERE s.medium <> 'organic'
 ),
 
-spendings as (
-    select
-        DATE(campaign_date) as campaign_date,
+spendings AS (
+    SELECT
+        DATE(campaign_date) AS campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(daily_spent) as total_cost
-    from vk_ads
-    group by 1, 2, 3, 4
-    union
-    select
-        DATE(campaign_date) as campaign_date,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY 1, 2, 3, 4
+    UNION
+    SELECT
+        DATE(campaign_date) AS campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(daily_spent) as total_cost
-    from ya_ads
-    group by 1, 2, 3, 4
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
 ),
 
-agg_tab as (
-    select
+agg_tab AS (
+    SELECT
         utm_source,
         utm_medium,
         utm_campaign,
-        DATE(visit_date) as visit_date,
-        COUNT(visitor_id) as visitors_count,
-        COUNT(lead_id) as leads_count,
-        COUNT(lead_id) filter (
-            where status_id = 142
-        ) as purchases_count,
-        SUM(amount) as revenue
-    from ranked_clicks
-    where rn = 1
-    group by 4, 1, 2, 3
-    order by
-        8 desc nulls last, 4, 5 desc, 1 asc, 2 asc, 3 asc
+        DATE(visit_date) AS visit_date,
+        COUNT(visitor_id) AS visitors_count,
+        COUNT(lead_id) AS leads_count,
+        COUNT(lead_id) FILTER (
+            WHERE status_id = 142
+        ) AS purchases_count,
+        SUM(amount) AS revenue
+    FROM ranked_clicks
+    WHERE rn = 1
+    GROUP BY 4, 1, 2, 3
+    ORDER BY
+        8 DESC NULLS LAST, 4, 5 DESC, 1 ASC, 2 ASC, 3 ASC
 ),
 
-tab as (
-    select
+tab AS (
+    SELECT
         agg_tab.visit_date,
         agg_tab.utm_source,
         agg_tab.utm_medium,
@@ -154,53 +154,53 @@ tab as (
         agg_tab.leads_count,
         agg_tab.purchases_count,
         agg_tab.revenue
-    from agg_tab
-    inner join spendings as sp
-        on
-            agg_tab.utm_source = spendings.utm_source
-            and agg_tab.utm_medium = spendings.utm_medium
-            and agg_tab.utm_campaign = spendings.utm_campaign
-            and agg_tab.visit_date = spendings.campaign_date
-    order by 9 desc nulls last, 1, 5 desc, 2, 3, 4
+    FROM agg_tab
+    INNER JOIN spendings AS sp
+        ON
+            agg_tab.utm_source = sp.utm_source
+            AND agg_tab.utm_medium = sp.utm_medium
+            AND agg_tab.utm_campaign = sp.utm_campaign
+            AND agg_tab.visit_date = sp.campaign_date
+    ORDER BY 9 DESC NULLS LAST, 1, 5 DESC, 2, 3, 4
 )
 
-select
+SELECT
     utm_source,
-    case
-        when SUM(visitors_count) = 0 then 0
-        else ROUND(SUM(total_cost) / SUM(visitors_count), 2)
-    end as cpu,
-    case
-        when SUM(leads_count) = 0 then 0
-        else ROUND(SUM(total_cost) / SUM(leads_count), 2)
-    end as cpl,
-    case
-        when SUM(purchases_count) = 0 then 0
-        else ROUND(SUM(total_cost) / SUM(purchases_count), 2)
-    end as cppu,
+    CASE
+        WHEN SUM(visitors_count) = 0 THEN 0
+        ELSE ROUND(SUM(total_cost) / SUM(visitors_count), 2)
+    END AS cpu,
+    CASE
+        WHEN SUM(leads_count) = 0 THEN 0
+        ELSE ROUND(SUM(total_cost) / SUM(leads_count), 2)
+    END AS cpl,
+    CASE
+        WHEN SUM(purchases_count) = 0 THEN 0
+        ELSE ROUND(SUM(total_cost) / SUM(purchases_count), 2)
+    END AS cppu,
     ROUND(
         100.0 * (SUM(revenue) - SUM(total_cost)) / SUM(total_cost), 2
-    ) as roi
-from tab
-group by 1;
+    ) AS roi
+FROM tab
+GROUP BY 1;
 
 -- Затраты
-select
+SELECT
 	campaign_date::date,
 	utm_source,
 	utm_medium,
 	utm_campaign,
 	sum(daily_spent) as total_cost
-from vk_ads
-group by 1, 2, 3, 4
-order by 1
-union 
-select
+FROM vk_ads
+GROUP BY 1, 2, 3, 4
+ORDER BY 1
+UNION
+SELECT
 	campaign_date::date,
 	utm_source,
 	utm_medium,
 	utm_campaign,
 	sum(daily_spent) as total_cost
-from ya_ads
-group by 1, 2, 3, 4
-order by 1;
+FROM ya_ads
+GROUP BY 1, 2, 3, 4
+ORDER BY 1;
